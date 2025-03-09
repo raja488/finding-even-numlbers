e803609d2c6a839966e46fe925949bddf5d1186a
umar <umarqadoos93@gmail.com>
2025-03-09 18:46:50 +0000

    homeowkr

--- mian1.py
+++ mian1.py
@@ -49,6 +49,21 @@ print(friends.count("asad"))
 print(friends.count("ali"))
 
 
+fruits = ["strawberry", "kiwi", "banana", "lemon", "watermelon", "tomato", "orange", "berrier", "plum", "blueberry"]
+
+
+for fruit in fruits:
+    print(f"Fruit: {fruit}")
+    for letter in fruit:
+        print(letter)
+
+print(fruits.count(fruit))
+print(len(fruits))
+
+
+
+
+
 
     
     
